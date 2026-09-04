d0359284d9f65e1cfa4b0ac487577cac4c6a2410
ZeEngoleSeco <mkaua0135@gmail.com>
2026-09-03 22:02:33 -0300

    mais alterações na aula 03/09/2026

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,39 +1,141 @@
-import { useState } from "react";
-import { Link } from "react-router";
+import { use, useState } from "react";
+import { Link, useNavigate } from "react-router";
 
 function Auth() {
 
     const [email, setEmail] = useState ("");
     const [senha, setSenha] = useState ("");
+    const [msg, setMsg] = useState ("");
+
+    const nav = useNavigate()
+    
+    function handleRegister(){
+        const users = JSON.parse(localStorage.getItem('users'))
+
+        let user = users.find(u =>{
+            return u.email ==email
+        })
+
+        if(!user){
+            console.log("Usuário não reconhecido")
+            return
+        }
+
+        if(user.senha == senha){
+            console.log("Usuário logado")
+            localStorage.setItem("users", JSON.stringify(user))
+            nav("/painel")
+        }else{
+            console.log("Senha inválida")
+        }
+    }
 
     return (
 
-        <div className="min-h-screen bg-black/70 text-white">
-            <nav className="fixed z-50 w-full bg-[#1c1d24]/95 backdrop-blur-md shadow-lg">
-                <div className="mx-auto px-6 py-4 flex">
-                    <Link to="/" className="py-2 px-4 bg-purple-700 text-white rounded-full shadow hover:bg-secondary">
+        <div className="
+        min-h-screen
+        bg-black/70
+        text-white
+        ">
+            <nav className="
+            fixed
+            z-50
+            w-full
+            bg-[#1c1d24]/95
+            backdrop-blur-md
+            shadow-lg
+            ">
+                <div className="
+                mx-auto 
+                px-6 
+                py-4 
+                flex">
+                    <Link to="/"
+                    className="
+                    py-2 
+                    px-4 
+                    bg-purple-700 
+                    text-white 
+                    rounded-full 
+                    shadow 
+                    hover:bg-purple-600 
+                    hover:shadow-purple-900/50 
+                    transition-all 
+                    duration-300 
+                    hover:scale-[1.02] 
+                    active:scale-[0.98]
+                    ">
                         Voltar
                     </Link>
                 </div>
             </nav>
         
-            <main className="min-h-screen flex items-center justify-center px-4 pt-20">
+            <main className="
+            min-h-screen
+            flex 
+            items-center 
+            justify-center 
+            px-4 
+            pt-20
+            ">
 
                 
-            <div className=" relative w-full max-w-md">
-                <div className="absolute inset-0 bg-purple-700/20 blur-3xl rounded-full"></div>
-                    <div className="text-center mb-8">
-                        <h1 className="text-4xl font-bold">
+            <div className=" 
+                relative 
+                w-full 
+                max-w-md
+            ">
+                <div className="
+                absolute
+                inset-0
+                z-0 
+                pointer-events-none 
+                bg-purple-700/20 
+                blur-3xl 
+                rounded-full
+                "></div>
+                <div className="
+                relative
+                z-10 
+                mb-12
+                ">
+                    <div className="
+                    text-center 
+                    mb-8
+                    ">
+                        <h1 className="
+                        text-4xl 
+                        font-bold
+                        ">
                             Login
                         </h1>
-                        <p className="mt-2 text-gray-400">
+                        <p className="
+                        mt-2 
+                        text-gray-400
+                        ">
                             Entre na sua conta para continuar
                         </p>
                     </div>
-                    
-                    <form id="form_login" className="w-full p-8 bg-[#080808] border border-purple-900/40 rounded-2xl shadow-2xl">
+                    <span>
+                        {msg}
+                    </span>
+                    <form id="form_login" 
+                    className="w-full 
+                    p-8 bg-[#080808] 
+                    border 
+                    border-purple-900/40 
+                    rounded-2xl 
+                    shadow-2xl
+                    ">
                         <div className="mb-6">
-                            <label htmlFor="i_emaillogin" className="block mb-2 text-sm font-medium text-gray-200">
+                            <label htmlFor="i_emaillogin" 
+                            className="
+                            block 
+                            mb-2 
+                            text-sm 
+                            font-medium 
+                            text-gray-200
+                            ">
                                 Email
                             </label>
                             <input
@@ -41,7 +143,21 @@ function Auth() {
                                 type="email"
                                 value={email}
                                 placeholder="Digite seu email"
-                                className="w-full px-4 py-3 bg-[#17171c] border border-gray-700 rounded-xl text-white outline-none transition duration-300 focus:border-purple-600 focus:ring-2 focus:ring-purple-600/20 placeholder:text-gray-500"
+                                className="w-full 
+                                px-4 
+                                py-3 
+                                bg-[#17171c] 
+                                border 
+                                border-gray-700 
+                                rounded-xl 
+                                text-white 
+                                outline-none 
+                                transition 
+                                duration-300 
+                                focus:border-purple-600 
+                                focus:ring-2 
+                                focus:ring-purple-600/20 
+                                placeholder:text-gray-500"
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                             {email}
@@ -49,7 +165,11 @@ function Auth() {
                         <div className="mb-4">
                             <label
                                 htmlFor="i_passwordlogin"
-                                className="block mb-2 text-sm font-medium text-gray-200"
+                                className="
+                                block mb-2 
+                                text-sm 
+                                font-medium 
+                                text-gray-200"
                             >
                                 Senha
                             </label>
@@ -58,20 +178,44 @@ function Auth() {
                                 type="password"
                                 value={senha}
                                 placeholder="Digite sua senha"
-                                className="w-full px-4 py-3 bg-[#17171c] border border-gray-700 rounded-xl text-white outline-none transition duration-300 focus:border-purple-600 focus:ring-2 focus:ring-purple-600/20 placeholder:text-gray-500"
+                                className="w-full 
+                                px-4 
+                                py-3 
+                                bg-[#17171c] 
+                                border 
+                                border-gray-700 
+                                rounded-xl 
+                                text-white 
+                                outline-none 
+                                transition 
+                                duration-300 
+                                focus:border-purple-600 
+                                focus:ring-2 
+                                focus:ring-purple-600/20 
+                                placeholder:text-gray-500"
                                 onChange={(e) => setSenha(e.target.value)}
                             />
                             {senha}
                         </div>
-                        <Link
-                            id="button_login"
+                        <a
+                            onClick={handleRegister}
                             type="submit"
-                            className="w-full py-3 rounded-xl bg-purple-700 text-white font-semibold shadow-lg transition-all duration-300 hover:bg-purple-600 hover:shadow-purple-900/40 hover:scale-[1.02] active:scale-[0.98]"
+                            className="
+                            w-full 
+                            py-3 rounded-xl 
+                            bg-purple-700 text-white 
+                            font-semibold shadow-lg 
+                            transition-all duration-300 
+                            hover:bg-purple-600 
+                            hover:shadow-purple-900/40 
+                            hover:scale-[1.02] 
+                            active:scale-[0.98]"
                             to="/painel"
                         >
                             Login
-                        </Link>
+                        </a>
                     </form>
+                </div>
                 </div>
             </main>
         </div>
